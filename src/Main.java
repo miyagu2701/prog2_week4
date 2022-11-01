34200e14fddc718026ab8f6d06ece0659bf15e40
Miyaguni Mikihiro <e225753@ie.u-ryukyu.ac.jp>
2022-11-01 09:53:40 +0900

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("main method runs");
+        Chatbot bot1 = new Chatbot();
+        Chatbot bot2 = new Chatbot("naltoma");
+        bot1.greeting();
+        bot2.greeting();
+        bot2.name = "ほげ";
+        bot2.greeting();
+    } 
+}
